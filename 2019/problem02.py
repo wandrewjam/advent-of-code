334ad145caa84f34c7c4ab3cda68fb9e652ecb91
Andrew Watson <awatson@math.utah.edu>
2019-12-03 07:16:44 -0700

    Solve day 02, part 2

--- 2019/problem02.py
+++ 2019/problem02.py
@@ -9,11 +9,16 @@ def load_file(filename: str) -> list:
     return program
 
 
-def main(program: list):
+def write_input(program: list, noun: int = 12, verb: int = 2) -> list:
+    program[1] = noun
+    program[2] = verb
+    return program
+
+
+def run_program(program: list) -> int:
     for i in range(len(program) // 4 + 1):
         opcode = program[4 * i]
         if opcode == 99:
-            print('Break!')
             break
         pos1, pos2, pos3 = program[(4 * i + 1):(4 * i + 4)]
         if opcode == 1:
@@ -22,8 +27,28 @@ def main(program: list):
             program[pos3] = program[pos1] * program[pos2]
         else:
             raise ValueError('Invalid opcode!')
+    return program[0]
 
-    print(program)
+
+def search_input_space(saved_program: list) -> tuple:
+    for noun in range(100):
+        for verb in range(100):
+            program_copy = [i for i in saved_program]
+            write_input(program_copy, noun, verb)
+            out = run_program(program_copy)
+            if out == 19690720:
+                return noun, verb
+
+
+def main(program: list):
+    saved_program = [i for i in program]
+    write_input(program)
+    out = run_program(program)
+    print(out)
+
+    noun, verb = search_input_space(saved_program)
+
+    print(100 * noun + verb)
 
 
 if __name__ == '__main__':
@@ -33,7 +58,5 @@ if __name__ == '__main__':
         int_code = load_file(sys.argv[1])
     else:
         int_code = load_file('problem02.in')
-        int_code[1] = 12
-        int_code[2] = 2
 
     main(int_code)
